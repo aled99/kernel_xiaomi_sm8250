1cb5cac9ec9cfb05205b59b716a270d434a7d2da
ale99 <bumbi@live.it>
2023-12-13 19:28:45 +0100

    input_boost: fix

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -112,7 +112,7 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		freq = max(max_boost_freq_little, cpu_freq_min_little);
-	else if (cpumask_test_cpu(policy->cpu, cpu_hp_mask))
+	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
 		freq = max(max_boost_freq_big, cpu_freq_min_big);
         else
                 freq = max(max_boost_freq_prime, cpu_freq_min_prime);
@@ -125,7 +125,7 @@ static unsigned int get_min_freq(struct cpufreq_policy *policy)
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		freq = cpu_freq_min_little;
-	else if (cpumask_test_cpu(policy->cpu, cpu_hp_mask))
+	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
 		freq = cpu_freq_min_big;
         else
                 freq = cpu_freq_min_prime;
@@ -138,7 +138,7 @@ static unsigned int get_idle_freq(struct cpufreq_policy *policy)
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		freq = cpu_freq_idle_little;
-	else if (cpumask_test_cpu(policy->cpu, cpu_hp_mask))
+	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
 		freq = cpu_freq_idle_big;
         else
                 freq = cpu_freq_idle_prime;
@@ -154,7 +154,7 @@ static void update_online_cpu_policy(void)
 	get_online_cpus();
 	cpu = cpumask_first_and(cpu_lp_mask, cpu_online_mask);
 	cpufreq_update_policy(cpu);
-	cpu = cpumask_first_and(cpu_hp_mask, cpu_online_mask);
+	cpu = cpumask_first_and(cpu_perf_mask, cpu_online_mask);
 	cpufreq_update_policy(cpu);
 	cpu = cpumask_first_and(cpu_prime_mask, cpu_online_mask);
 	cpufreq_update_policy(cpu);
