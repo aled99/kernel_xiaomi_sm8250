d3946c1f38beca2a3817ffcc2584fae68a62ff84
aled99 <bumbi@live.it>
2024-02-16 10:38:11 +0100

    techpack: msm: Reduce latency while completing non-blocking commits
    In order to complete a commit, we must first wait for the previous
    commit to finish up. This is done by sleeping, during which time the CPU
    can enter a deep idle state and take a while to finish processing the
    commit after the wait is over. We can alleviate this by optimistically
    assuming that the kthread this commit worker is running on won't migrate
    to another CPU partway through. We only do this for the non-blocking
    case where the commit completion is done in an asynchronous worker
    because the generic DRM code already does this for atomic ioctls. Since
    non-blocking commits are the common case, preemptively kick the prime
    CPU out of idle when the atomic ioctl runs so that it'll be ready to
    process the DRM IRQ and display commit kthreads.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [EmanuelCN: Adapt to k4.19]

--- techpack/display/msm/msm_atomic.c
+++ techpack/display/msm/msm_atomic.c
@@ -544,16 +544,22 @@ static void _msm_drm_commit_work_cb(struct kthread_work *work)
 {
 	struct msm_commit *commit = container_of(work, typeof(*commit),
 						 commit_work);
-	ktime_t start, end;
-	s64 duration;
 	struct pm_qos_request req = {
 		.type = PM_QOS_REQ_AFFINE_CORES,
 		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()) |
 					   *cpumask_bits(cpu_prime_mask))
 	};
-        commit = container_of(work, struct msm_commit, commit_work);
+        ktime_t start, end;
+        s64 duration;
         start = ktime_get();
-	frame_stat_collector(0, COMMIT_START_TS);
+        frame_stat_collector(0, COMMIT_START_TS);
+
+
+	/*
+	 * Optimistically assume the current task won't migrate to another CPU
+	 * and restrict the current CPU to shallow idle states so that it won't
+	 * take too long to resume after waiting for the prior commit to finish.
+	 */
 	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
 	SDE_ATRACE_BEGIN("complete_commit");
 	complete_commit(commit);
