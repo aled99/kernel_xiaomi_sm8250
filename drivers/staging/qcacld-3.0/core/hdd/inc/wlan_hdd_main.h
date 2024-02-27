903e3359d3cf0c4faa7698f04dfab9f99c6936a1
aled99 <bumbi@live.it>
2024-02-27 14:59:26 +0100

    Revert "qcacld-3.0: Control GRO en/dis based on TC filter prio"
    This reverts commit da23690a0b08b8e5537286b52cdf295f6a843ad4.

--- drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_main.h
+++ drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_main.h
@@ -1140,20 +1140,6 @@ struct rcpi_info {
 
 struct hdd_context;
 
-#ifdef WLAN_FEATURE_DYNAMIC_RX_AGGREGATION
-/**
- * enum qdisc_filter_status - QDISC filter status
- * @QDISC_FILTER_RTNL_LOCK_FAIL: rtnl lock acquire failed
- * @QDISC_FILTER_PRIO_MATCH: qdisc filter with priority match
- * @QDISC_FILTER_PRIO_MISMATCH: no filter match with configured priority
- */
-enum qdisc_filter_status {
-	QDISC_FILTER_RTNL_LOCK_FAIL,
-	QDISC_FILTER_PRIO_MATCH,
-	QDISC_FILTER_PRIO_MISMATCH,
-};
-#endif
-
 /**
  * struct hdd_adapter - hdd vdev/net_device context
  * @vdev: object manager vdev context
@@ -1480,7 +1466,7 @@ struct hdd_adapter {
 	bool handle_feature_update;
 
 	qdf_work_t netdev_features_update_work;
-	qdf_atomic_t gro_disallowed;
+	uint8_t gro_disallowed[DP_MAX_RX_THREADS];
 	uint8_t gro_flushed[DP_MAX_RX_THREADS];
 	bool delete_in_progress;
 	qdf_atomic_t net_dev_hold_ref_count[NET_DEV_HOLD_ID_MAX];
@@ -1796,10 +1782,10 @@ struct hdd_adapter_ops_history {
  * @sar_cmd_params: SAR command params to be configured to the FW
  * @rx_aggregation: rx aggregation enable or disable state
  * @gro_force_flush: gro force flushed indication flag
- * @tc_based_dyn_gro: TC based dynamic GRO enable/disable flag
- * @tc_ingress_prio: TC ingress priority
+ * @force_gro_enable: force GRO enable or disable flag
  * @adapter_ops_wq: High priority workqueue for handling adapter operations
  * @is_dual_mac_cfg_updated: indicate whether dual mac cfg has been updated
+ * @rx_skip_qdisc_chk_conc: flag to skip ingress qdisc check in concurrency
  */
 struct hdd_context {
 	struct wlan_objmgr_psoc *psoc;
@@ -2126,8 +2112,7 @@ struct hdd_context {
 	struct {
 		qdf_atomic_t rx_aggregation;
 		uint8_t gro_force_flush[DP_MAX_RX_THREADS];
-		bool tc_based_dyn_gro;
-		uint32_t tc_ingress_prio;
+		bool force_gro_enable;
 	} dp_agg_param;
 #ifdef FW_THERMAL_THROTTLE_SUPPORT
 	uint8_t dutycycle_off_percent;
@@ -2136,6 +2121,7 @@ struct hdd_context {
 	qdf_workqueue_t *adapter_ops_wq;
 	struct hdd_adapter_ops_history adapter_ops_history;
 	bool is_dual_mac_cfg_updated;
+	qdf_atomic_t rx_skip_qdisc_chk_conc;
 };
 
 /**
