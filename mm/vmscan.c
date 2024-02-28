9b2738b675b9227fa2e786e865c66f852a6ca6a6
aled99 <bumbi@live.it>
2024-02-28 23:25:28 +0100

    Revert "mm: vmscan: Declare local variables before statement declarations"
    This reverts commit 1e8ed2d85274668906cec5271316f898454e0f12.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1840,7 +1840,7 @@ static unsigned long isolate_lru_pages(unsigned long nr_to_scan,
  */
 int isolate_lru_page(struct page *page)
 {
-	int ret = -EBUSY, lru = page_lru(page);
+	int ret = -EBUSY;
 
 	VM_BUG_ON_PAGE(!page_count(page), page);
 	WARN_RATELIMIT(PageTail(page), "trying to isolate tail page");
