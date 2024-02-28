a713eaef6ac0b766bc30a1efeb7011970c909ab9
aled99 <bumbi@live.it>
2024-02-28 23:32:46 +0100

    fpc: update
    and revert cnss2

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee_apollo.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee_apollo.c
@@ -797,26 +797,6 @@ static ssize_t irq_ack(struct device *dev, struct device_attribute *attr,
 
 static DEVICE_ATTR(irq, S_IRUSR | S_IWUSR, irq_get, irq_ack);
 
-static ssize_t fingerdown_wait_set(struct device *dev,
-				   struct device_attribute *attr,
-				   const char *buf, size_t count)
-{
-	struct fpc1020_data *fpc1020 = dev_get_drvdata(dev);
-
-	dev_info(fpc1020->dev, "%s -> %s\n", __func__, buf);
-	if (!strncmp(buf, "enable", strlen("enable")) && fpc1020->prepared)
-		fpc1020->wait_finger_down = true;
-	else if (!strncmp(buf, "disable", strlen("disable")) &&
-		 fpc1020->prepared)
-		fpc1020->wait_finger_down = false;
-	else
-		return -EINVAL;
-
-	return count;
-}
-
-static DEVICE_ATTR(fingerdown_wait, S_IWUSR, NULL, fingerdown_wait_set);
-
 static ssize_t vendor_update(struct device *dev, struct device_attribute *attr,
 			     const char *buf, size_t count)
 {
